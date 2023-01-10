cb7585f927704eda66ef5d4654019ac32e8a8d72
yokubariTETSUsama-Sadame <116971771+yokubariTETSUsama-Sadame@users.noreply.github.com>
2023-01-10 20:04:41 +0800

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,951 @@
+#include <stdio.h>
+
+int main()
+{
+    char a, b, c, d;
+
+    scanf("%c%c%c%c", &a, &b, &c, &d);
+
+    switch (a)
+    {
+    case '0':
+        printf("*****");
+        break;
+    case '1':
+        printf("    *");
+        break;
+    case '2':
+        printf("*****");
+        break;
+    case '3':
+        printf("*****");
+        break;
+    case '4':
+        printf("*   *");
+        break;
+    case '5':
+        printf("*****");
+        break;
+    case '6':
+        printf("*****");
+        break;
+    case '7':
+        printf("*****");
+        break;
+    case '8':
+        printf("*****");
+        break;
+    case '9':
+        printf("*****");
+        break;
+    }
+    printf(" ");
+    switch (b)
+    {
+    case '0':
+        printf("*****");
+        break;
+    case '1':
+        printf("    *");
+        break;
+    case '2':
+        printf("*****");
+        break;
+    case '3':
+        printf("*****");
+        break;
+    case '4':
+        printf("*   *");
+        break;
+    case '5':
+        printf("*****");
+        break;
+    case '6':
+        printf("*****");
+        break;
+    case '7':
+        printf("*****");
+        break;
+    case '8':
+        printf("*****");
+        break;
+    case '9':
+        printf("*****");
+        break;
+    }
+    printf(" ");
+    switch (c)
+    {
+    case '0':
+        printf("*****");
+        break;
+    case '1':
+        printf("    *");
+        break;
+    case '2':
+        printf("*****");
+        break;
+    case '3':
+        printf("*****");
+        break;
+    case '4':
+        printf("*   *");
+        break;
+    case '5':
+        printf("*****");
+        break;
+    case '6':
+        printf("*****");
+        break;
+    case '7':
+        printf("*****");
+        break;
+    case '8':
+        printf("*****");
+        break;
+    case '9':
+        printf("*****");
+        break;
+    }
+    printf(" ");
+    switch (d)
+    {
+    case '0':
+        printf("*****\n");
+        break;
+    case '1':
+        printf("    *\n");
+        break;
+    case '2':
+        printf("*****\n");
+        break;
+    case '3':
+        printf("*****\n");
+        break;
+    case '4':
+        printf("*   *\n");
+        break;
+    case '5':
+        printf("*****\n");
+        break;
+    case '6':
+        printf("*****\n");
+        break;
+    case '7':
+        printf("*****\n");
+        break;
+    case '8':
+        printf("*****\n");
+        break;
+    case '9':
+        printf("*****\n");
+        break;
+    }
+    /********************************************************/
+
+    switch (a)
+    {
+    case '0':
+        printf("*   *");
+        break;
+    case '1':
+        printf("    *");
+        break;
+    case '2':
+
+        printf("    *");
+
+        break;
+    case '3':
+
+        printf("    *");
+
+        break;
+    case '4':
+
+        printf("*   *");
+
+        break;
+    case '5':
+
+        printf("*    ");
+
+        break;
+    case '6':
+
+        printf("*    ");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*   *");
+
+        break;
+    case '9':
+
+        printf("*   *");
+
+        break;
+    }
+    printf(" ");
+    switch (b)
+    {
+    case '0':
+        printf("*   *");
+        break;
+    case '1':
+        printf("    *");
+        break;
+    case '2':
+
+        printf("    *");
+
+        break;
+    case '3':
+
+        printf("    *");
+
+        break;
+    case '4':
+
+        printf("*   *");
+
+        break;
+    case '5':
+
+        printf("*    ");
+
+        break;
+    case '6':
+
+        printf("*    ");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*   *");
+
+        break;
+    case '9':
+
+        printf("*   *");
+
+        break;
+    }
+    printf(" ");
+    switch (c)
+    {
+    case '0':
+        printf("*   *");
+        break;
+    case '1':
+        printf("    *");
+        break;
+    case '2':
+
+        printf("    *");
+
+        break;
+    case '3':
+
+        printf("    *");
+
+        break;
+    case '4':
+
+        printf("*   *");
+
+        break;
+    case '5':
+
+        printf("*    ");
+
+        break;
+    case '6':
+
+        printf("*    ");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*   *");
+
+        break;
+    case '9':
+
+        printf("*   *");
+
+        break;
+    }
+    printf(" ");
+    switch (d)
+    {
+    case '0':
+        printf("*   *\n");
+        break;
+    case '1':
+        printf("    *\n");
+        break;
+    case '2':
+
+        printf("    *\n");
+
+        break;
+    case '3':
+
+        printf("    *\n");
+
+        break;
+    case '4':
+
+        printf("*   *\n");
+
+        break;
+    case '5':
+
+        printf("*    \n");
+
+        break;
+    case '6':
+
+        printf("*    \n");
+
+        break;
+    case '7':
+
+        printf("    *\n");
+
+        break;
+    case '8':
+
+        printf("*   *\n");
+
+        break;
+    case '9':
+
+        printf("*   *\n");
+
+        break;
+    }
+    /**************************************************************/
+    switch (a)
+    {
+    case '0':
+
+        printf("*   *");
+
+        break;
+    case '1':
+
+        printf("    *");
+
+        break;
+    case '2':
+
+        printf("*****");
+
+        break;
+    case '3':
+
+        printf("*****");
+
+        break;
+    case '4':
+
+        printf("*****");
+
+        break;
+    case '5':
+
+        printf("*****");
+
+        break;
+    case '6':
+
+        printf("*****");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*****");
+
+        break;
+    case '9':
+
+        printf("*****");
+
+        break;
+    }
+    printf(" ");
+    switch (b)
+    {
+    case '0':
+
+        printf("*   *");
+
+        break;
+    case '1':
+
+        printf("    *");
+
+        break;
+    case '2':
+
+        printf("*****");
+
+        break;
+    case '3':
+
+        printf("*****");
+
+        break;
+    case '4':
+
+        printf("*****");
+
+        break;
+    case '5':
+
+        printf("*****");
+
+        break;
+    case '6':
+
+        printf("*****");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*****");
+
+        break;
+    case '9':
+
+        printf("*****");
+
+        break;
+    }
+    printf(" ");
+    switch (c)
+    {
+    case '0':
+
+        printf("*   *");
+
+        break;
+    case '1':
+
+        printf("    *");
+
+        break;
+    case '2':
+
+        printf("*****");
+
+        break;
+    case '3':
+
+        printf("*****");
+
+        break;
+    case '4':
+
+        printf("*****");
+
+        break;
+    case '5':
+
+        printf("*****");
+
+        break;
+    case '6':
+
+        printf("*****");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*****");
+
+        break;
+    case '9':
+
+        printf("*****");
+
+        break;
+    }
+    printf(" ");
+    switch (d)
+    {
+    case '0':
+
+        printf("*   *\n");
+
+        break;
+    case '1':
+
+        printf("    *\n");
+
+        break;
+    case '2':
+
+        printf("*****\n");
+
+        break;
+    case '3':
+
+        printf("*****\n");
+
+        break;
+    case '4':
+
+        printf("*****\n");
+
+        break;
+    case '5':
+
+        printf("*****\n");
+
+        break;
+    case '6':
+
+        printf("*****\n");
+
+        break;
+    case '7':
+
+        printf("    *\n");
+
+        break;
+    case '8':
+
+        printf("*****\n");
+
+        break;
+    case '9':
+
+        printf("*****\n");
+
+        break;
+    }
+    /**********************************************************/
+    switch (a)
+    {
+    case '0':
+
+        printf("*   *");
+
+        break;
+    case '1':
+
+        printf("    *");
+
+        break;
+    case '2':
+
+        printf("*    ");
+
+        break;
+    case '3':
+
+        printf("    *");
+
+        break;
+    case '4':
+        printf("    *");
+
+        break;
+    case '5':
+
+        printf("    *");
+
+        break;
+    case '6':
+
+        printf("*   *");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*   *");
+
+        break;
+    case '9':
+
+        printf("    *");
+
+        break;
+    }
+    printf(" ");
+    switch (b)
+    {
+    case '0':
+
+        printf("*   *");
+
+        break;
+    case '1':
+
+        printf("    *");
+
+        break;
+    case '2':
+
+        printf("*    ");
+
+        break;
+    case '3':
+
+        printf("    *");
+
+        break;
+    case '4':
+        printf("    *");
+
+        break;
+    case '5':
+
+        printf("    *");
+
+        break;
+    case '6':
+
+        printf("*   *");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*   *");
+
+        break;
+    case '9':
+
+        printf("    *");
+
+        break;
+    }
+    printf(" ");
+    switch (c)
+    {
+    case '0':
+
+        printf("*   *");
+
+        break;
+    case '1':
+
+        printf("    *");
+
+        break;
+    case '2':
+
+        printf("*    ");
+
+        break;
+    case '3':
+
+        printf("    *");
+
+        break;
+    case '4':
+        printf("    *");
+
+        break;
+    case '5':
+
+        printf("    *");
+
+        break;
+    case '6':
+
+        printf("*   *");
+
+        break;
+    case '7':
+
+        printf("    *");
+
+        break;
+    case '8':
+
+        printf("*   *");
+
+        break;
+    case '9':
+
+        printf("    *");
+
+        break;
+    }
+    printf(" ");
+    switch (d)
+    {
+    case '0':
+
+        printf("*   *\n");
+
+        break;
+    case '1':
+
+        printf("    *\n");
+
+        break;
+    case '2':
+
+        printf("*    \n");
+
+        break;
+    case '3':
+
+        printf("    *\n");
+
+        break;
+    case '4':
+        printf("    *\n");
+
+        break;
+    case '5':
+
+        printf("    *\n");
+
+        break;
+    case '6':
+
+        printf("*   *\n");
+
+        break;
+    case '7':
+
+        printf("    *\n");
+
+        break;
+    case '8':
+
+        printf("*   *\n");
+
+        break;
+    case '9':
+
+        printf("    *\n");
+
+        break;
+    }
+    /********************************************************/
+    switch (a)
+    {
+    case '0':
+
+        printf("*****");
+        break;
+    case '1':
+
+        printf("    *");
+        break;
+    case '2':
+
+        printf("*****");
+        break;
+    case '3':
+
+        printf("*****");
+        break;
+    case '4':
+
+        printf("    *");
+        break;
+    case '5':
+
+        printf("*****");
+        break;
+    case '6':
+
+        printf("*****");
+        break;
+    case '7':
+
+        printf("    *");
+        break;
+    case '8':
+
+        printf("*****");
+        break;
+    case '9':
+
+        printf("    *");
+        break;
+    }
+    printf(" ");
+    switch (b)
+    {
+    case '0':
+
+        printf("*****");
+        break;
+    case '1':
+
+        printf("    *");
+        break;
+    case '2':
+
+        printf("*****");
+        break;
+    case '3':
+
+        printf("*****");
+        break;
+    case '4':
+
+        printf("    *");
+        break;
+    case '5':
+
+        printf("*****");
+        break;
+    case '6':
+
+        printf("*****");
+        break;
+    case '7':
+
+        printf("    *");
+        break;
+    case '8':
+
+        printf("*****");
+        break;
+    case '9':
+
+        printf("    *");
+        break;
+    }
+    printf(" ");
+    switch (c)
+    {
+    case '0':
+
+        printf("*****");
+        break;
+    case '1':
+
+        printf("    *");
+        break;
+    case '2':
+
+        printf("*****");
+        break;
+    case '3':
+
+        printf("*****");
+        break;
+    case '4':
+
+        printf("    *");
+        break;
+    case '5':
+
+        printf("*****");
+        break;
+    case '6':
+
+        printf("*****");
+        break;
+    case '7':
+
+        printf("    *");
+        break;
+    case '8':
+
+        printf("*****");
+        break;
+    case '9':
+
+        printf("    *");
+        break;
+    }
+    printf(" ");
+    switch (d)
+    {
+    case '0':
+
+        printf("*****\n");
+        break;
+    case '1':
+
+        printf("    *\n");
+        break;
+    case '2':
+
+        printf("*****\n");
+        break;
+    case '3':
+
+        printf("*****\n");
+        break;
+    case '4':
+
+        printf("    *\n");
+        break;
+    case '5':
+
+        printf("*****\n");
+        break;
+    case '6':
+
+        printf("*****\n");
+        break;
+    case '7':
+
+        printf("    *\n");
+        break;
+    case '8':
+
+        printf("*****\n");
+        break;
+    case '9':
+
+        printf("    *\n");
+        break;
+    }
+
+    return 0;
+}
